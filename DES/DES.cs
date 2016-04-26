ff7ebb04d4d70c32793883c6bc796ce6f7849f9a
irtazaku <irtazaku@hotmail.com>
2016-04-26 22:19:53 +0500

    masking added

--- DES/DES.cs
+++ DES/DES.cs
@@ -13,11 +13,46 @@ namespace DES
         public bool[][] pre = new bool[17][];
         public bool[][] sk = new bool[16][];
         int[] round = {1,1,2,2,2,2,2,2,1,2,2,2,2,2,2,1};
+        int[] pc1 = { 57, 49, 41, 33, 25, 17, 9, 1, 58, 50, 42, 34, 26, 18, 10, 2, 59, 51, 43, 35, 27, 19, 11, 3, 60, 52, 44, 36, 63, 55, 47, 39, 31, 23, 15, 7, 62, 54, 46, 38, 30, 22, 14, 6, 61, 53, 45, 37, 29, 21, 13, 5, 28, 20, 12, 4 };
+        int[] pc2 = { 14, 17, 11, 24, 1, 5, 3, 28, 15, 6, 21, 10, 23, 19, 12, 4, 26, 8, 16, 7, 27, 20, 13, 2, 41, 52, 31, 37, 47, 55, 30, 40, 51, 45, 33, 48, 44, 49, 39, 56, 34, 53, 46, 42, 50, 36, 29, 32 };
         int pointer = 0;
+
         public DES(string k, string msg)
         {
             strToDeci(k);
         }
+        
+        public void keygen ()
+        {
+            bool[] temp = masking(pc1, key);
+            bool[][] K = new bool[16][];
+            for (int r = 0; r < 16; r++)
+            {
+                temp = prepc2( r);
+                K[r] = masking(pc2, pre[r]);
+                for (int i = 0; i < K[r].Length; i++)
+                {
+                    if (K[r][i])
+                        Console.Write("1");
+                    else
+                        Console.Write("0");
+                }
+                Console.WriteLine();
+            }
+        }
+
+        public bool[] masking(int[] arr, bool[] arr2)
+        { 
+            bool[] temp = new bool[arr.Length];
+            
+            for (int i = 0; i < arr.Length; i++)
+            {
+                temp[i] = arr2[arr[i] - 1];
+            }
+            pre[0] = temp;
+                return temp;
+        }
+
 
         public bool[] prepc2(int r)
         {
@@ -29,11 +64,11 @@ namespace DES
                 tempL[i] = pre[r][i];
             }
             
-            int[] roundtable = { 1, 1, 2, 2, 2, 2, 2, 2, 1, 2, 2, 2, 2, 2, 2, 1 };
+            
             for (int i = 0; i < 28; i++)
             {
-                pre[r][i] = tempL[(i + roundtable[r]) % 28];
-                pre[r][i + 28] = tempR[((i + roundtable[r]) % 28)];
+                pre[r][i] = tempL[(i + round[r]) % 28];
+                pre[r][i + 28] = tempR[((i + round[r]) % 28)];
 
             }
             pre[r + 1] = pre[r];
@@ -42,7 +77,7 @@ namespace DES
         public bool[] pc1table()
         {
             bool[] temp = new bool[56];
-            int[] pc1 = { 57, 49, 41, 33, 25, 17, 9, 1, 58, 50, 42, 34, 26, 18, 10, 2, 59, 51, 43, 35, 27, 19, 11, 3, 60, 52, 44, 36, 63, 55, 47, 39, 31, 23, 15, 7, 62, 54, 46, 38, 30, 22, 14, 6, 61, 53, 45, 37, 29, 21, 13, 5, 28, 20, 12, 4 };
+            
             for (int i = 0; i < 56; i++)
             {
                 temp[i] = key[pc1[i]-1];
@@ -54,7 +89,7 @@ namespace DES
         {
             bool[] temp=this.pre[r];
             sk[r] = new bool[48];
-            int[] pc2 = { 14, 17, 11, 24, 1, 5, 3, 28, 15, 6, 21, 10, 23, 19, 12, 4, 26, 8, 16, 7, 27, 20, 13, 2, 41, 52, 31, 37, 47, 55, 30, 40, 51, 45, 33, 48, 44, 49, 39, 56, 34, 53, 46, 42, 50, 36, 29, 32 };
+            
             for (int i = 0; i < 48; i++)
             {
                 sk[r][i] = temp[pc2[i] - 1];
--- DES/Program.cs
+++ DES/Program.cs
@@ -13,21 +13,7 @@ namespace DES
         static void Main(string[] args)
         {
             DES d = new DES("133457799BBCDFF1", "");
-            bool[] temp = d.pc1table();
-            bool[][] K = new bool[16][];
-            for (int r = 0; r < 16; r++)
-            {
-                temp = d.prepc2( r);
-                K[r] = d.pc2table(r);
-                for (int i = 0; i < 48; i++)
-                {
-                    if (K[r][i])
-                        Console.Write("1");
-                    else
-                        Console.Write("0");
-                }
-                Console.WriteLine();
-            }
+            d.keygen();
         }
     }
 }
